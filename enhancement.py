bc14c0cdb0723017e73d3bcc2ad2a1db462fc2cc
Julius Richter <jrichter@informatik.uni-hamburg.de>
2020-10-29 12:54:33 +0100

    add utils

--- enhancement.py
+++ enhancement.py
@@ -19,33 +19,33 @@ test_data = pickle.load(open('data/mixture.p', 'rb'))
 
 ############################## VAE #############################################
 
-vae_model_path = 'models/vae_200_vloss_0476.pt'
+# vae_model_path = 'models/vae_200_vloss_0476.pt'
 
-vae = VAE(in_out_dim=513, hid_dim=128, latent_dim=16, 
-    num_hid_layers=2).to(device)
-vae.load_state_dict(torch.load(vae_model_path))
-vae.eval()
-for param in vae.parameters(): param.requires_grad = False
+# vae = VAE(in_out_dim=513, hid_dim=128, latent_dim=16, 
+#     num_hid_layers=2).to(device)
+# vae.load_state_dict(torch.load(vae_model_path))
+# vae.eval()
+# for param in vae.parameters(): param.requires_grad = False
 
-s_hat = []
-win = np.sin(np.arange(.5,1024-.5+1)/1024*np.pi) 
+# s_hat = []
+# win = np.sin(np.arange(.5,1024-.5+1)/1024*np.pi) 
 
-for i, x in enumerate(test_data):
-    print('File {}/{}'.format(i+1,len(test_data)), end="\r")
-    x = x/np.max(x)
-    T_orig = len(x)
+# for i, x in enumerate(test_data):
+#     print('File {}/{}'.format(i+1,len(test_data)), end="\r")
+#     x = x/np.max(x)
+#     T_orig = len(x)
     
-    Y = stft(x, n_fft=1024, hop_length=256, win_length=1024, window=win)
+#     Y = stft(x, n_fft=1024, hop_length=256, win_length=1024, window=win)
     
-    mcem = MCEM(Y, vae, device)
-    mcem.run()
-    mcem.separate(niter_MH=100, burnin=75)
+#     mcem = MCEM(Y, vae, device)
+#     mcem.run()
+#     mcem.separate(niter_MH=100, burnin=75)
 
-    S_hat = mcem.S_hat + np.finfo(np.float32).eps
-    s_hat.append(istft(stft_matrix=S_hat, hop_length=256, win_length=1024, 
-        window=win, length=T_orig))
+#     S_hat = mcem.S_hat + np.finfo(np.float32).eps
+#     s_hat.append(istft(stft_matrix=S_hat, hop_length=256, win_length=1024, 
+#         window=win, length=T_orig))
     
-pickle.dump(s_hat, open('data/pickle/s_hat_vae.p', 'wb'), protocol=4)
+# pickle.dump(s_hat, open('data/pickle/s_hat_vae.p', 'wb'), protocol=4)
 
 
 ############################## STCN ############################################
---
+++ utils.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
